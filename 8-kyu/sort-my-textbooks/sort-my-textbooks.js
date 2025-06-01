87446ea9c3068e2ab312468939520dd6c3f52945
Alice <higiroalice99@gmail.com>
2025-06-01 12:25:48 +0300

    Update solution - CodeHub

--- 8-kyu/sort-my-textbooks/sort-my-textbooks.js
+++ 8-kyu/sort-my-textbooks/sort-my-textbooks.js
@@ -1,6 +1,6 @@
  
 function sorter(textbooks) {
   let books=['math','physics','computer science','english']
-  books.sort((a,b)=> a.toLowerCase().localeCompare(b.toLowerCase))
+  books.sort((a,b)=> a.toUpperCase().localeCompare(b.toUpperCase))
   return textbooks;
 }
