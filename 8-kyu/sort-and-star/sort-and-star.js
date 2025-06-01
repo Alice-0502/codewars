b295b055208e8e2c1e6ddd9b9e8a0699fdae0a9f
Alice <higiroalice99@gmail.com>
2025-06-01 12:05:50 +0300

    Update solution - CodeHub

--- 8-kyu/sort-and-star/sort-and-star.js
+++ 8-kyu/sort-and-star/sort-and-star.js
@@ -1,6 +1,6 @@
 function twoSort(s) {
   let names= ['Alice','Flavienne','Kenia']
   names.sort((a,b)=> a.localeCompare(b));
-  s[0].sort().split('').join('***');
+  s.sort()[0].split('').join('***');
 ​
 }
